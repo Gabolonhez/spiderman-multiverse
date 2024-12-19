b08c18c4efdb2931b07acdcb7b2d3ffb6bb600b4
Gabriel BF <gbbolonhez@gmail.com>
2024-12-18 23:41:42 -0300

    uploading

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,21 +1,18 @@
 function handleMouseEnter() {
-  this.classList.add('s-card--hovered');
-  document.body.id = `${this.id}-hovered`;
+  this.classList.add("s-card--hovered");
 }
 
 function handleMouseLeave() {
-  this.classList.remove('s-card--hovered');
-  document.body.id = '';
+  this.classList.remove("s-card--hovered");
 }
 
-function addEventListenersToCards() {
-  const cardElements = document.getElementsByClassName('s-card');
-  
-  for (let index = 0; index < cardElements.length; index++) {
-    const card = cardElements[index];
-    card.addEventListener('mouseenter', handleMouseEnter);
-    card.addEventListener('mouseleave', handleMouseLeave);
-  }
+function addEventListenersToCard() {
+  const cardElements = document.getElementsByClassName("s-card");
+
+  Array.from(cardElements).forEach(card => {
+    card.addEventListener("mouseenter", handleMouseEnter);
+    card.addEventListener("mouseleave", handleMouseLeave);
+  });
 }
 
-document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+document.addEventListener("DOMContentLoaded", addEventListenersToCard(), false);
